944d3f80e1ae0a7fa5ab5afa5c817612230ef845
uNBEx <you@example.com>
2019-12-07 18:30:46 +0100

    happysad

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <ul v-if="numbers.length > 0">
+      <li :key="number" v-for="number in numbers">{{ number }}</li>
+    </ul>
+    <button @click="addNew()">Add new random number</button>
   </div>
 </template>
-
+ 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        data() {
+            return {numbers: []}
+        },
+        methods: {
+            addNew() {
+                this.numbers.push(Math.round(Math.random() * 100));
+            }
+        }
+    };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
