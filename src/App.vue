ed03faaf11830f187eca06db821c6842225de979
Student <student@d17-l427-pc-16.ipa.ki.agh.edu.pl>
2019-12-09 09:03:13 +0100

    cos

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,41 @@
 <template>
   <div>
-    <ul v-if="numbers.length > 0">
-      <li :key="number" v-for="number in numbers">{{ number }}</li>
-    </ul>
-    <button @click="addNew()">Add new random number</button>
+    <div>
+      <h1>Participants list</h1>
+      <ol>
+        <li :key="string" v-for="string in osoby">{{string}}</li>
+      </ol>
+    </div>
+
+    <h3>New participant</h3>
+    <form @submit.prevent="addNewParticipant()">
+      <label>Firstname</label>
+      <input type="text" v-model="imie">
+      <label>Lastname</label>
+      <input type="text" v-model="nazwisko">
+      <button>Add new participant</button>
+    </form>
   </div>
 </template>
- 
+
 <script>
-    export default {
-        data() {
-            return {numbers: []}
-        },
-        methods: {
-            addNew() {
-                this.numbers.push(Math.round(Math.random() * 100));
-            }
+  export default {
+    data() {
+      return {
+        osoby:[],
+        imie:"",
+        nazwisko:"",
+      };
+    },
+    methods: {
+      addNewParticipant() {
+        if(this.imie!=""&&this.nazwisko!="")
+        {
+        this.osoby.push(this.imie+" "+this.nazwisko);
+        this.imie="";
+        this.nazwisko="";
         }
-    };
+      }
+    }
+  };
 </script>
